6062f6d164e455e98e47f6aa8c904d67f2a65215
Shamme Akter <shammeakter786@gmail.com>
2025-05-13 17:03:42 +0600

    r Basic part-02

---
+++ RDSML-Day-04 R Programming Basics (Part-02)/rbasics_part_02.R
@@ -0,0 +1,53 @@
+# Data Types in R
+# 1. Numeric - Integer/Floating point
+# 2. String/ Character - 'Shamme Akter'/ "Shamme Akter"
+# 3. Logical
+
+a = 11
+b =7.19
+class(a)
+class(b)
+
+name = "Shamme Akter"
+print(name)
+class(name)
+
+c = "52"
+
+print(a+9)
+class(c)
+
+# Logical Data Type - TRUE,FALSE (T,F)
+a>b
+class(a>b)
+a<b
+x =19
+y = 22-3
+print(x==y)
+print(x!=y)
+print(y>x)
+print(y>=x)
+
+# Logical operators in R
+# && logical operator
+# || logical operator
+
+4==4
+4==4 &&5==5
+x!=y ||x==y
+
+# Simple Mathematical operator in R
+z = 6+9-8*2/3
+print(z)
+
+w = 4+3-3*2/3
+print(w)
+
+y = 4^2
+print(y)
+y = 4^2
+print(y)
+
+11/5
+11%%5
+11 %/% 5
